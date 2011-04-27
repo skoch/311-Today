7a31b934bbf21337edf551070d659b69861dde0f
Stephen Koch <s.koch@bigspaceship.com>
2011-04-27 10:18:34 -0400

    cleaned up code

--- NYCParking/NYCParkingAppDelegate.m
+++ NYCParking/NYCParkingAppDelegate.m
@@ -47,7 +47,6 @@
 	/*
 	 Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 	 */
-	[self.mainViewController removeWebView];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
